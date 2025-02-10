17d40e518a51b0d6dd828e1f427e096c4beb754a
이관현 <rhksgus509@gmail.com>
2025-02-11 01:47:51 +0900

    Add files via upload

---
+++ coding33.js
@@ -0,0 +1,15 @@
+let num = prompt('숫자를 입력하세요.');
+
+console.log(num.split(' ').reverse().join(' '));
+
+//모범답안
+
+// let nums = "1 2 3 4 5";
+// let arr = nums.split(" ").reverse();
+// let reverseVal ="";
+
+// for(let i = 0; i < arr.length; i++){
+//   reverseVal = reverseVal + arr[i]; 
+// }
+
+// console.log(reverseVal);
